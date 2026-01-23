73dce358b54c71b956a6eafa0ff21cc4be8d92c7
simon <micle-qin@live.com>
2026-01-23 18:03:27 +0800

    feat: normalize detected language

--- packages/frontend/src/i18n/index.ts
+++ packages/frontend/src/i18n/index.ts
@@ -1,7 +1,7 @@
 import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import { SUPPORTED_LANGUAGES } from './languages';
+import { normalizeLanguage, SUPPORTED_LANGUAGES } from './languages';
 import { resources } from './resources';
 
 i18next
@@ -14,7 +14,11 @@ i18next
         ns: ['common', 'auth'],
         defaultNS: 'common',
         interpolation: { escapeValue: false },
-        detection: { order: ['navigator'] },
+        detection: {
+            order: ['navigator'],
+            convertDetectedLanguage: (language) =>
+                normalizeLanguage(language) ?? language,
+        },
     });
 
 export default i18next;
